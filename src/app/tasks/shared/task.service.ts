7746de7b55f0e812305abfa8054c9efb5b5683b1
willamy388720 <willamygt@gmail.com>
2020-05-28 15:55:12 -0300

    Adding #getTask to TaskService

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -34,4 +34,9 @@ export class TaskService {
     return Promise.resolve(TASKS.slice(0, 3));
   }
 
+  public getTask(id: number): Promise<Task> {
+    return this.getTasks()
+      .then(tasks => tasks.find(task => task.id === id))
+  }
+
 }
